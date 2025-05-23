636984f78fff9c75ff98b224676daf6fe496a082
Sofia Amaya <laura18amaya@gmail.com>
2025-05-22 22:24:36 -0500

    migration and domain partI

---
+++ ShoppingAPI/Domain/Interfaces/ICountryService.cs
@@ -0,0 +1,11 @@
+﻿using ShoppingAPI.DAL.Entities;
+
+namespace ShoppingAPI.Domain.Interfaces
+{
+    public interface ICountryService
+    {
+        Task<IEnumerable<Country>> GetCountriesAsync();
+        Task<Country> CreateCountryAsync(Country country);
+
+    }
+}
