da7082b41efa1061f69f44c028e1b25ab55aedb2
Sofia Amaya <laura18amaya@gmail.com>
2025-06-08 07:43:27 -0500

    feat agregar seedDb

--- ShoppingAPI/DAL/SeederDb.cs
+++ ShoppingAPI/DAL/SeederDb.cs
@@ -9,6 +9,83 @@
             _context = context;
         }
 
+        public async Task SeederAsync()
+        { 
+            await _context.Database.EnsureCreatedAsync();
 
+            await PopulateCountriesAsync();
+        }
+
+        #region Private Methods
+
+        private async Task PopulateCountriesAsync()
+        {
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Entities.Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Colombia",
+                    States = new List<Entities.State>
+                    {
+                        new Entities.State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Antioquia"
+                        },
+                        new Entities.State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Caldas"
+                        },
+                        new Entities.State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Cundinamarca"
+                        }
+                    }
+                });
+
+                _context.Countries.Add(new Entities.Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Perú",
+                    States = new List<Entities.State>
+                    {
+                        new Entities.State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Lima"
+                        },
+                        new Entities.State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Arequipa"
+                        },
+                        new Entities.State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Cusco"
+                        }
+                    }
+                });
+
+                _context.Countries.Add(new Entities.Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Argentina",
+                    States = new List<Entities.State>
+                    {
+                        new Entities.State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Buenos Aires"
+                        }
+                    }
+                });
+            }
+            
+        }
+        #endregion
     }
 }
--- ShoppingAPI/Program.cs
+++ ShoppingAPI/Program.cs
@@ -13,14 +13,28 @@ builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(b
 
 builder.Services.AddScoped<ICountryService, CountryService>();
 builder.Services.AddScoped<IStateService, StateService>();
+builder.Services.AddTransient<SeederDb>();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
+SeederData();
+
+void SeederData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    { 
+        SeederDb? service = scope.ServiceProvider.GetService<SeederDb>();
+        service.SeederAsync().Wait();
+    }
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
